9a0344f23974279307fa4b300d4ad60f03239775
ducdinh-codehub <dinhduc31197@gmail.com>
2022-06-25 15:55:17 +0700

    Testing email client system

--- src/main/java/com/mycompany/javamail/main.java
+++ src/main/java/com/mycompany/javamail/main.java
@@ -6,12 +6,41 @@
 package com.mycompany.javamail;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 /**
  *
  * @author Duc
  */
 public class main {
-    public static void main(String args[]){
+    public static String user;
+    public static String pass;
+    public static JTextField ReAcc = new JTextField(35);
+    public static JTextField Cc = new JTextField(35);
+    public static JTextArea Content = new JTextArea();
+    public static void main(String args[]) throws FileNotFoundException, IOException{
+        File file = new File("D:\\java_small_project\\javaMail\\src\\main\\java\\com\\mycompany\\javamail\\accInfor.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String str;
+        
+        while((str = br.readLine()) != null){
+            user = str.split(" ")[0];
+            pass = str.split(" ")[1];            
+        }
+        
         System.out.println("Hello world");
         JFrame mainFrame = new JFrame("Main frame");
         
@@ -25,46 +54,70 @@ public class main {
         GridBagLayout layout = new GridBagLayout();
         mainPanel.setLayout(layout);
         
-        JTextField MyAccount = new JTextField(35);
         
-        JLabel MyAccountLabel = new JLabel();
-        MyAccountLabel.setText("Your account");
-        JTextField MyPassword = new JPasswordField(35);
-        JLabel MyPasswordLabel = new JLabel();
-        MyPasswordLabel.setText("Your password");
+        
+        JLabel ReAccLabel = new JLabel();
+        ReAccLabel.setText("Account");
+
         JLabel CcLabel = new JLabel();
         CcLabel.setText("CC");
-        JTextField Cc = new JTextField(35);
+        
         JLabel ContentLabel = new JLabel();
         ContentLabel.setText("Content");
-        JTextArea Content = new JTextArea();
+        
         Content.setColumns(35);
         JButton SendingBtb = new JButton();
         SendingBtb.setText("Sending");
+        SendingBtb.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Properties prop = new Properties();
+                prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+                prop.put("mail.smtp.auth", true);
+                prop.put("mail.smtp.starttls.enable", true);
+                prop.put("mail.smtp.host", "smtp.gmail.com");
+                prop.put("mail.smtp.port", "587");
+                
+                Session sess =  Session.getInstance(prop, new javax.mail.Authenticator(){
+                    protected javax.mail.PasswordAuthentication getPasswordAuthentication(){
+                        return new javax.mail.PasswordAuthentication(user,pass);
+                    }
+                });
+                
+                try{
+                    /*
+                    ReAcc
+                    Cc
+                    Content
+                    */
+                    Message mess = new MimeMessage(sess);
+                    mess.setFrom(new InternetAddress("no-reply@gmail.com"));
+                    mess.setRecipients(Message.RecipientType.TO, InternetAddress.parse(ReAcc.getText()));
+                    mess.setSubject(Cc.getText());
+                    mess.setText(Content.getText());
+                    
+                    Transport.send(mess);
+                    
+                    System.out.println("Success");
+                    
+                    
+                }
+                catch(Exception g){
+                    System.out.println(g);
+                }
+            }
+        });
         
         gbc.weighty = 0.1;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 0;
         gbc.gridy = 0;
-        mainPanel.add(MyAccountLabel, gbc);
+        mainPanel.add(ReAccLabel, gbc);
         
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 1;
         gbc.gridy = 0;
-        mainPanel.add(MyAccount, gbc);
-        
-        gbc.weighty = 0.1;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        gbc.gridheight = 1;
-        mainPanel.add(MyPasswordLabel, gbc);
-        
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.gridx = 1;
-        gbc.gridy = 1;
-        gbc.gridheight = 1;
-        mainPanel.add(MyPassword, gbc);
+        mainPanel.add(ReAcc, gbc);
         
         gbc.weighty = 0.1;
         gbc.fill = GridBagConstraints.HORIZONTAL;
